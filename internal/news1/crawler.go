73136316b10b36db7549ac284fda4b3db6d30a50
ywy <3251589147@qq.com>
2025-08-21 19:58:12 +0800

    feat: add crawler

---
+++ internal/news1/crawler.go
@@ -0,0 +1,54 @@
+package news1
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"news_helper/model"
+
+	"github.com/uozi-tech/cosy/queue"
+
+	"github.com/uozi-tech/cosy/logger"
+)
+
+var (
+	NewsCrawlerQueue *queue.Queue[model.News]
+)
+
+func InitQueue() {
+	NewsCrawlerQueue = queue.New[model.News]("news_crawler", queue.LeftToRight)
+}
+
+func CrawlHtml(method, url string, headers http.Header, queryParams url.Values, data interface{}) ([]byte, error) {
+	var reqBody io.Reader
+	if data != nil {
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			return nil, err
+		}
+		reqBody = bytes.NewReader(jsonData)
+	}
+	// 1. 发起 HTTP 请求
+	req, err := http.NewRequest(method, url, reqBody)
+	if err != nil {
+		return nil, err
+	}
+	req.Header = headers
+	req.URL.RawQuery = queryParams.Encode()
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		logger.Errorf("请求失败:", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		logger.Errorf("HTTP 错误: %d", resp.StatusCode)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}

007ded77396dfa6dae7fc5951bf49a583e6e548d
ywy <3251589147@qq.com>
2025-08-21 19:58:49 +0800

    feat: add cosy base

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"flag"
-	"news_helper/router"
-	"news_helper/settings"
+
+	"news_helper/internal/audit"
+	"news_helper/internal/limiter"
+	"news_helper/internal/news1"
+
+	"git.uozi.org/uozi/burn-api/model"
+	"git.uozi.org/uozi/burn-api/model/view"
+	"git.uozi.org/uozi/burn-api/query"
+	"git.uozi.org/uozi/burn-api/router"
+	"github.com/uozi-tech/cosy"
+	mysql "github.com/uozi-tech/cosy-driver-mysql"
+	"github.com/uozi-tech/cosy/settings"
 )
 
 type Config struct {
@@ -14,13 +24,36 @@ type Config struct {
 var cfg Config
 
 func init() {
+	// 指定默认配置文件，命令行与参数
 	flag.StringVar(&cfg.ConfPath, "config", "app.ini", "Specify the configuration file")
+	// 解析
 	flag.Parse()
 }
 
 func main() {
-	settings.InitDatabase(*settings.DataBaseSettings)
-	settings.InitRedis(*settings.RedisSettings)
-	router.Init()
+	// 注册模型，用于数据库迁移和查询等
+	cosy.RegisterModels(model.GenerateAllModel()...)
+	// 注册初始化函数，数据库连接实例化、创建admin用户，email:admin;password:admin初始化查询模块、实例化数据库对象传递给模型。创建数据库视图，初始化限流器，控制请求速率
+	cosy.RegisterInitFunc(func() {
+		db := cosy.InitDB(mysql.Open(settings.DataBaseSettings))
+		query.Init(db)
+		model.Use(db)
+		view.CreateViews(db)
+		limiter.Init()
+	},
+		// 初始化运行时配置，初始化路由
+		model.InitRuntimeSettings,
+		router.InitRouter,
+	)
+	// 注册后台运行函数，用于异步运行收集日志和监控
+	cosy.RegisterGoroutine(
+		audit.Init,
+		news1.InitNews,
+	)
+
+	// cosy启动
+	cosy.Boot(cfg.ConfPath)
+	// tmpemail := "123@qq.com"
+	// Register1(tmpemail)
 
 }
